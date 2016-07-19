ab4bb775cf837d06d37ce9aaab0b8667ae8b1efd
Sumner Caughey <saboten@gmail.com>
2016-07-19 15:26:52 -0400

    require_tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,6 @@
 //= require ./app
 //= require ./home
 //= require ./index
-//= require_tree ./controllers/
-//= require_tree ./directives/
-//= require_tree ./filters/
-//= require_tree ./services/
-//= require_tree ./factories/
+//= require_tree ./controllers
+//= require_tree ./directives
+//= require_tree ./factories
